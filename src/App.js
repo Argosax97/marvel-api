6b769a6babf67ac22317b116bb273c25367bd14c
Argosax97 <alex.hakobyan.1@gmail.com>
2021-12-28 17:53:10 +0400

    Marvel's api begining of the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/categories/Home";
+import ErrorPage from "./components/error_page/ErrorPage";
+import Characters from "./components/categories/Characters";
+import Comics from "./components/categories/Comics";
+import Creators from "./components/categories/Creators";
+import Events from "./components/categories/Events";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/characters" element={<Characters />} />
+        <Route path="/comics" element={<Comics />} />
+        <Route path="/creators" element={<Creators />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
